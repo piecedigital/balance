387cade3c83efcb70dd7cd6abbf402255d2c722f
piecedigital <piecedigitalstudios@gmail.com>
2016-05-26 00:31:15 -0400

    added logging module

---
+++ custom_modules/error_logger.js
@@ -0,0 +1,21 @@
+var fs = require("fs"),
+path = require("path");
+
+module.exports = function(file, okayLogFile, errorLogFile) {
+  if(!file || !okayLogFile || !errorLogFile) throw new Error("!!! Log file paths required");
+  return {
+    ok(message) {
+      writeLog(message, file, okayLogFile)
+    },
+    error(message) {
+      writeLog(message, file, errorLogFile);
+    }
+  };
+};
+
+function writeLog(message, file, filePath) {
+  var data = `[Date=${new Date()}; File=${file}; User=${process.env["USER"]}] ${message}\n\r`;
+  fs.appendFile(path.join(__dirname, filePath), data, function(err) {
+    if(err) console.error(new Error(err).stack);
+  });
+};
