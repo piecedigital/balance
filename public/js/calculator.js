98643e168f3d278a2fa23ac60dcd718451fc1b14
piecedigital <piecedigitalstudios@gmail.com>
2016-05-28 22:55:09 -0400

    developing calculator.js

--- public/js/calculator.js
+++ public/js/calculator.js
@@ -13,20 +13,19 @@ calculator = {
       expense: 0,
       net: 0,
       grossBySource: {},
-      expenseBySource: {},
-      netBySource: {}
+      expenseBySource: {}//,
+      // netBySource: {}
     };
 
     var calc = function(record) {
-      financialData[record].map(function(record) {
-        if(returnData.grossBySource[record.sourceName.sourceNameFlat]) {
-          returnData.grossBySource[record.sourceName.sourceNameFlat].money += record.money;
+      financialData[record].map(function(value) {
+        if(returnData.grossBySource[value.sourceName.sourceNameFlat]) {
+          returnData.grossBySource[value.sourceName.sourceNameFlat].money += value.money;
         } else {
-          returnData.grossBySource[record.sourceName.sourceNameFlat] = {
-            sourceName: record.sourceName.sourceName,
-            sourceNameFlat: record.sourceName.sourceNameFlat,
-            money: record.money,
-            date: record.date
+          returnData.grossBySource[value.sourceName.sourceNameFlat] = {
+            sourceName: value.sourceName.sourceName,
+            sourceNameFlat: value.sourceName.sourceNameFlat,
+            money: value.money
           };
         };
         returnData.gross += returnData.grossBySource[record.sourceName.sourceNameFlat].money;
@@ -36,6 +35,17 @@ calculator = {
       calc(key);
     });
     // gross, expense, grossBySource and expenseBySource should be completed by now
+    returnData.net = gross - expense;
+    // gross, expense, net, grossBySource and expenseBySource should be completed by now
+
+    // grossBySource.map(function(value, key) {
+    //   returnData.netBySource[value.sourceName.sourceNameFlat] = {
+    //     sourceName: value.sourceName.sourceName,
+    //     sourceNameFlat: value.sourceName.sourceNameFlat,
+    //     money: value.money * (value.money * returnData.gross)
+    //   };
+    // });
+    return returnData;
   }
 };
 
